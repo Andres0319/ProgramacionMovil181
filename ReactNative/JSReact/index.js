c734ad2eb5e7128923abf6b637355f8364cbafb5
Andres0319 <121038655@upq.edu.mx>
2024-06-11 19:25:16 -0600

    .

---
+++ ReactNative/JSReact/index.js
@@ -0,0 +1,22 @@
+
+
+
+console.log('hola consola') 
+/* Funciones */
+function suma(a,b){
+    return a+b
+}
+
+console.log(suma(1,10))
+/* Fat Arrow (Simplificar el uso de las funciones) */
+const resta=(a,b)=>a-b
+console.log(resta(100,10))
+
+/* Objetos */
+
+const usuario={
+    nombre:'Ana',
+    apellido:'González',
+    edad:30,
+    direccion:'Calle union numero 11'
+}
