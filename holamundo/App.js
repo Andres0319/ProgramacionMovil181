074cce0cea7c96a907cb0a6eac21aaed8a201f3e
Andres0319 <121038655@upq.edu.mx>
2024-06-18 20:24:31 -0600

    Componetes, Props y State

--- holamundo/App.js
+++ holamundo/App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+
+const Texto =(props)=>{
+  const [contenido, setContenido]= useState('Hola :)')
+  const actualizarTexto= ()=>{ setContenido('contenido actualizado ')}
+  return(
+    <Text onPress={actualizarTexto}> {contenido} </Text>
+  )
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Mundo ReactNative</Text>
+      <Texto> uno </Texto>
+      <Texto> dos </Texto>
+
+      <Texto/>
+
+      <Button title='saludar' color="#841584"></Button>
       <StatusBar style="auto" />
     </View>
   );
